27caa3e8eafb76f08defdb844a718ef8013aa0f7
raquelbezerrambm <raquel.bezerra.mbm@gmail.com>
2025-01-11 02:21:33 -0300

    Done Excercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Using a Random Generator
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,1001);
+
+        int guessedNumber = -1;
+
+        do
+        {
+            // Asking the user for the guess
+            Console.Write("What is your guess? ");
+            guessedNumber = int.Parse(Console.ReadLine());
+            
+            // If the guessed number is greater than the magic number
+            if (guessedNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        
+            // If the person guessed the correct number
+            else if (guessedNumber == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+            // If the guessed number is lower than the magic number
+            else
+        {
+            Console.WriteLine("Higher");
+        }
+        } while ( guessedNumber != magicNumber);
     }
 }
