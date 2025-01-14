0d254da9851f10ab75de5006d0f95f74f9dbc9d5
raquelbezerrambm <raquel.bezerra.mbm@gmail.com>
2025-01-14 10:42:32 -0300

    Modified the displayed content

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -11,13 +11,14 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
-        job1.JobInformation();
 
         Job job2 = new Job();
         job2._company = "SpaceX";
         job2._jobTitle = "Data Analyst";
         job2._startYear = 2022;
         job2._endYear = 2024;
-        job2.JobInformation();
+        
+        Console.WriteLine($"{job1._company}");
+        Console.WriteLine($"{job2._company}");
     }
 }
