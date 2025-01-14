411271a57cf0781163f0f2442437409748e7db83
raquelbezerrambm <raquel.bezerra.mbm@gmail.com>
2025-01-14 10:33:38 -0300

    Steps 1-4 of Activity Instructions concluded

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,15 @@
+public class Job
+{
+    // Responsibility: Keeps track of the company, job title, start year, and end year
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    // Behaviors: Displays the job information in the format "Job Title (Company) StartYear-EndYear"
+    public void JobInformation()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._company = "Meta";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        job1.JobInformation();
+
+        Job job2 = new Job();
+        job2._company = "SpaceX";
+        job2._jobTitle = "Data Analyst";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+        job2.JobInformation();
     }
 }
