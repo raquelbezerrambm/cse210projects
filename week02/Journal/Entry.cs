940bce16024404b963f64a0f31b835c36a42f05b
raquelbezerrambm <raquel.bezerra.mbm@gmail.com>
2025-01-16 00:23:19 -0300

    Started writing code for Journal Assignment

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,13 @@
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_date}");
+        Console.WriteLine($"{_promptText}");
+        Console.WriteLine($"{_entryText}");
+    }
+}
---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry[] newEntry)
+    {
+        // Add code here
+    }
+
+    public void DisplayAll()
+    {
+        // Add code here
+    }
+
+    public void SaveToFile(string[] file)
+    {
+        // Add code here
+    }
+
+    public void LoadFromFile(string[] file)
+    {
+        // Add code here
+    }
+}
